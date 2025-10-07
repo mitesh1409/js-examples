9bb9c2ed9a059afb471b0b041aafd283ff08591f
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-10-08 01:44:43 +0530

    Add example for ES6 - let

---
+++ examples/2/app.js
@@ -0,0 +1,18 @@
+// console.log(`Order total = ${orderTotal}`);
+// Uncaught ReferenceError: Cannot access 'orderTotal' before initialization
+
+// Lines 1, 2, 3 are temporal dead zone for orderTotal.
+// orderTotal is hoisted and allocated memory but you can't use it until it is initialized.
+let orderTotal = 0;
+
+// product worth 100 added to cart.
+orderTotal += 100;
+
+// product worth 250 added to cart.
+orderTotal += 250;
+
+console.log(`Order total = ${orderTotal}`);
+
+// Re-declaration is not allowed.
+// let orderTotal = 10;
+// Uncaught SyntaxError: Identifier 'orderTotal' has already been declared (at app.js:16:5)
