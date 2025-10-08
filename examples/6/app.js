798fda60c53cd41862f853a4795394c72e55bc04
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-10-08 14:27:34 +0530

    Add example for the Array Destructuring

---
+++ examples/6/app.js
@@ -0,0 +1,18 @@
+// Create an Array
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// Destructuring Assignment
+// Get some.
+// let [fruit1, fruit2] = fruits;
+
+// Get all.
+let [fruit1, fruit2, fruit3, fruit4, fruit5, fruit6] = fruits;
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+console.log(fruit4);
+
+// fruits array has 4 elements only, so fruit5 and fruit6 are undefined.
+console.log(fruit5);
+console.log(fruit6);
