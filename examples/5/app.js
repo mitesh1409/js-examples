2473ad01042709ea32bd127adf2a288c2f6457c3
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-10-09 21:42:39 +0530

    Update example

--- examples/5/app.js
+++ examples/5/app.js
@@ -18,5 +18,19 @@ console.log('lastName', lastName);
 console.log('age', age);
 console.log('eyeColor', eyeColor);
 
+// Changing values got from the destructuring.
+firstName = 'Peter';
+lastName = 'Parker';
+age = 21;
+eyeColor = "Black";
+
+console.log('firstName', firstName);
+console.log('lastName', lastName);
+console.log('age', age);
+console.log('eyeColor', eyeColor);
+
+// Original object still remains the same.
+console.log('Person', person);
+
 // dateOfBirth is not an Object property so it will be undefined.
 console.log('dateOfBirth', dateOfBirth);
